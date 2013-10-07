44614c0df59cebe5f0f5e07aa25ad652a47f41ab
Plux Devergo <plux.rt@gmail.com>
2013-10-07 23:30:58 +0500

    environment updated

--- emacs/emacsconfig.el
+++ emacs/emacsconfig.el
@@ -1,6 +1,16 @@
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(toggle-scroll-bar -1)
+; disable all unnecessary UI elements
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(toggle-scroll-bar 0)
+(setf inhibit-splash-screen t)
 
+; disable cursor blinking
+(blink-cursor-mode 0)
+
+; start with empty buffer
+(switch-to-buffer (get-buffer-create "emtpy"))
+(delete-other-windows)
+
+; set up language and font
 (set-language-environment "Russian")
 (set-default-font "Terminus-11")
