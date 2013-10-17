5cb05034e41edf1f80593129f5726a4b03aa5b47
Plux Devergo <plux.rt@gmail.com>
2013-10-17 23:31:55 +0500

    rdp visual studio blinking fixed

--- emacs/emacsconfig.el
+++ emacs/emacsconfig.el
@@ -1,8 +1,29 @@
+; fast access to the custom init file 
+(set-register ?e (cons 'file (concat custom-files-path "emacsconfig.el")))
+
+; OS type - are we running Microsoft Windows?
+(defvar running-ms-windows (eq system-type 'windows-nt))
+(defvar running-ms-windows (string-match "windows" (prin1-to-string system-type)))
+
+(custom-set-variables
+ '(custom-theme-directory (concat custom-files-path "themes"))
+ '(fringe-mode 0 nil (fringe))
+ '(indicate-buffer-boundaries nil)
+ '(indicate-empty-lines nil)
+ '(overflow-newline-into-fringe nil))
+
+; set default color theme
+(cond 
+  (running-ms-windows (load-theme 'pyte t))
+  (t (load-theme 'xoria))
+)
+
 ; disable all unnecessary UI elements
+(setq inhibit-startup-message t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (toggle-scroll-bar 0)
-(setf inhibit-splash-screen t)
+(setq left-fringe-width 0)
 
 ; disable cursor blinking
 (blink-cursor-mode 0)
@@ -13,4 +34,9 @@
 
 ; set up language and font
 (set-language-environment "Russian")
-(set-default-font "Terminus-11")
+(set-default-font 
+  (cond 
+    (running-ms-windows "Terminus-11")
+    (t "Terminus-9")
+  )
+)
